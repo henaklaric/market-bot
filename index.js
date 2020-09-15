333592ce3959b940939b42e8233e580b131a9bc4
hena <hena.klaric@gmail.com>
2020-09-15 12:59:18 +0200

    removed unnecessary dep

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict';
 const express = require('express');
-const bodyParser = require('body-parser');
 const monitor = require('./monitor.js');
 const config = require('./config.js');
 const cron = require('node-cron');
 
 let app = express();
-app.use(bodyParser.json());
 
 app.listen(config.port, function () {
     console.log('App started on port ' + config.port);
